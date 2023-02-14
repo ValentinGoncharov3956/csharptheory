f40569e7d757eb925e3300dc62ed343eae325861
Valik <valik.seal12@gmail.com>
2023-02-15 00:08:11 +0300

    5 test

---
+++ 5test/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "валик")
+{
+    Console.WriteLine("Ура, это же ВАЛИК");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
